c945f08a150fe9152ec54dea93d52da96d54368a
SOM <som_msk@promavto.net>
2023-06-03 00:18:19 +0900

    name

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -1,3 +1,3 @@
 from .admin import set_admin_commands, get_admin_commands
 from .default import set_default_commands, set_user_commands, get_default_commands
-from .iot import set_default_commands, set_user_commands, get_default_commands
+from .iot import set_default_commands, set_user_commands, get_iot_commands
